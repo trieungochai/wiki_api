44441bcb0a7dc42ef4302731764f47a620e083b3
trieungochai <trieungochai.dev@gmail.com>
2021-09-24 03:51:35 +0700

    Set Up Server

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/wikiDB", { useNewUrlParser: true });
+
+const articleSchema = new mongoose.Schema({
+  title: String,
+  content: String,
+});
+
+const Article = new mongoose.model("Article", articleSchema);
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000!");
+});
