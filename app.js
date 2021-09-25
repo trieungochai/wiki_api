1b79cf9ddc6292c5934fc60a63612da53199140a
trieungochai <trieungochai.dev@gmail.com>
2021-09-25 15:36:51 +0700

    PUT a Specific Article

--- app.js
+++ app.js
@@ -61,19 +61,32 @@ app
   });
 
 // Request Targeting a Specific Articles
-app.route("/articles/:articleTitle").get(function (req, res) {
-  // GET a Specific Article
-  Article.findOne(
-    { title: req.params.articleTitle },
-    function (err, foundTitle) {
-      if (foundTitle) {
-        res.send(foundTitle);
-      } else {
-        res.send("No articles matching that title was found!");
+app
+  .route("/articles/:articleTitle")
+  .get(function (req, res) {
+    Article.findOne(
+      { title: req.params.articleTitle },
+      function (err, foundTitle) {
+        if (foundTitle) {
+          res.send(foundTitle);
+        } else {
+          res.send("No articles matching that title was found!");
+        }
       }
-    }
-  );
-});
+    );
+  })
+  .put(function (req, res) {
+    Article.replaceOne(
+      { title: req.params.articleTitle },
+      { title: req.body.title, content: req.body.content },
+      { overwrite: true },
+      function (err) {
+        if (!err) {
+          res.send("Successfully updated article!");
+        }
+      }
+    );
+  });
 
 app.listen(3000, function () {
   console.log("Server started on port 3000!");
