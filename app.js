a33ba549e46ec2e869055d3d406ae4b9300e9b3d
trieungochai <trieungochai.dev@gmail.com>
2021-09-25 10:21:14 +0700

    DELETE ALL Articles

--- app.js
+++ app.js
@@ -48,6 +48,17 @@ app.post("/articles", function (req, res) {
   });
 });
 
+// DELETE all the articles
+app.delete("/articles", function (req, res) {
+  Article.deleteMany(function (err) {
+    if (!err) {
+      res.send("Successfully deleted all the articles.");
+    } else {
+      res.send(err);
+    }
+  });
+});
+
 app.listen(3000, function () {
   console.log("Server started on port 3000!");
 });
